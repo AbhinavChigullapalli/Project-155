5f4d720b9d406416c5d638e0067c2df5d7fd2727
AbhinavChigullapalli <86352418+AbhinavChigullapalli@users.noreply.github.com>
2023-03-02 16:42:19 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,20 @@
+AFRAME.registerComponent("game", {
+    schema:{
+        elementId:{type:"string", default:"#coin1"}
+    },
+    update:function(){
+        this.isCollided(this.data.elementId)
+    },
+    isCollided:function(elementId){        
+        const element = document.querySelector(elementId)
+        element.addEventListener("collide", e => {
+            if(elementId.includes("#coin")){
+                element.setAttribute("visible", false)
+                console.log("Coin collision")
+            }
+            else if(elementId.includes("#hurdle")){
+                console.log("Fish collision")
+            }
+        })
+    }
+})
